76556b59080e8f6f79103cd2262dd9c8b7dbd3aa
Colin Jones <colin.jones@phe.gov.uk>
2015-08-21 10:22:04 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lard::Application.config.secret_token = '45f8437c6dbbc403aa6002b7549136d8b8868b92213dc6630b138e1321b312328e93a5491676918d46a0fa8a9eba19f0bb8dd69a15750eb19891909a8fafa2e5'
